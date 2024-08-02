2e9d1950f0151051cf4a7bf996bb4a3a83c57ebc
Tiago Oliveira <contao@otao.dev.br>
2024-08-02 12:16:43 -0300

    feat: add created at field

--- src/Core/Domain/Entity/Traits/MagicMethodsTrait.php
+++ src/Core/Domain/Entity/Traits/MagicMethodsTrait.php
@@ -15,4 +15,9 @@ trait MagicMethodsTrait
 
         return $this->{$property};
     }
+
+    public function createdAt(string $format = 'Y-m-d H:i:s'): string
+    {
+        return $this->createdAt->format($format);
+    }
 }
