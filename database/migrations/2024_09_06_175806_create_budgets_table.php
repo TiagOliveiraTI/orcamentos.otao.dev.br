afe0b54f1fc4277bfcaae45a8c9e972cbe6c9d48
Tiago Oliveira <contato@otao.dev.br>
2024-09-06 15:29:24 -0300

    feat: create migrations

---
+++ database/migrations/2024_09_06_175806_create_budgets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('request_id')->constrained();
+            $table->decimal('total_amount');
+            $table->string('details');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('budgets');
+    }
+};
