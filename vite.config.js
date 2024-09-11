ed11e256bafe4cc859d00dfadf3872916c26f464
Tiago Aparecido de Oliveira <dev.tiago.oliveira@gmail.com>
2024-09-11 18:15:33 -0300

    Revert "Movido a importacao do arquivo css da configuracao do vite para o app…"

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    base: '/public/',
 });
